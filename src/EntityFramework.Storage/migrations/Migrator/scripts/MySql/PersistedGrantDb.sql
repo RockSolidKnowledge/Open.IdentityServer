6e0dfaa3096374b8d6f2ac078f896208521fd7b5
James Britton <james.britton@rocksolidknowledge.com>
2026-05-29 11:10:00 +0100

    fix: generated mysql scripts using pomelo package

--- src/EntityFramework.Storage/migrations/Migrator/scripts/MySql/PersistedGrantDb.sql
+++ src/EntityFramework.Storage/migrations/Migrator/scripts/MySql/PersistedGrantDb.sql
@@ -1,71 +1,73 @@
 ﻿CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
-    `MigrationId` varchar(150) NOT NULL,
-    `ProductVersion` varchar(32) NOT NULL,
-    PRIMARY KEY (`MigrationId`)
-);
+    `MigrationId` varchar(150) CHARACTER SET utf8mb4 NOT NULL,
+    `ProductVersion` varchar(32) CHARACTER SET utf8mb4 NOT NULL,
+    CONSTRAINT `PK___EFMigrationsHistory` PRIMARY KEY (`MigrationId`)
+) CHARACTER SET=utf8mb4;
 
 START TRANSACTION;
+ALTER DATABASE CHARACTER SET utf8mb4;
+
 CREATE TABLE `DeviceCodes` (
-    `UserCode` varchar(200) NOT NULL,
-    `DeviceCode` varchar(200) NOT NULL,
-    `SubjectId` varchar(200) NULL,
-    `SessionId` varchar(100) NULL,
-    `ClientId` varchar(200) NOT NULL,
-    `Description` varchar(200) NULL,
+    `UserCode` varchar(200) CHARACTER SET utf8mb4 NOT NULL,
+    `DeviceCode` varchar(200) CHARACTER SET utf8mb4 NOT NULL,
+    `SubjectId` varchar(200) CHARACTER SET utf8mb4 NULL,
+    `SessionId` varchar(100) CHARACTER SET utf8mb4 NULL,
+    `ClientId` varchar(200) CHARACTER SET utf8mb4 NOT NULL,
+    `Description` varchar(200) CHARACTER SET utf8mb4 NULL,
     `CreationTime` datetime(6) NOT NULL,
     `Expiration` datetime(6) NOT NULL,
-    `Data` longtext NOT NULL,
-    PRIMARY KEY (`UserCode`)
-);
+    `Data` longtext CHARACTER SET utf8mb4 NOT NULL,
+    CONSTRAINT `PK_DeviceCodes` PRIMARY KEY (`UserCode`)
+) CHARACTER SET=utf8mb4;
 
 CREATE TABLE `Keys` (
-    `Id` varchar(255) NOT NULL,
+    `Id` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
     `Version` int NOT NULL,
-    `Use` longtext NULL,
+    `Use` longtext CHARACTER SET utf8mb4 NULL,
     `DataProtected` tinyint(1) NOT NULL,
-    `Algorithm` varchar(100) NOT NULL,
+    `Algorithm` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
     `IsX509Certificate` tinyint(1) NOT NULL,
-    `Data` varchar(max) NOT NULL,
+    `Data` longtext CHARACTER SET utf8mb4 NOT NULL,
     `Created` datetime(6) NOT NULL,
-    PRIMARY KEY (`Id`)
-);
+    CONSTRAINT `PK_Keys` PRIMARY KEY (`Id`)
+) CHARACTER SET=utf8mb4;
 
 CREATE TABLE `PersistedGrants` (
     `Id` bigint NOT NULL AUTO_INCREMENT,
-    `Key` varchar(200) NULL,
-    `Type` varchar(50) NOT NULL,
-    `SubjectId` varchar(200) NULL,
-    `SessionId` varchar(100) NULL,
-    `ClientId` varchar(200) NOT NULL,
-    `Description` varchar(200) NULL,
+    `Key` varchar(200) CHARACTER SET utf8mb4 NULL,
+    `Type` varchar(50) CHARACTER SET utf8mb4 NOT NULL,
+    `SubjectId` varchar(200) CHARACTER SET utf8mb4 NULL,
+    `SessionId` varchar(100) CHARACTER SET utf8mb4 NULL,
+    `ClientId` varchar(200) CHARACTER SET utf8mb4 NOT NULL,
+    `Description` varchar(200) CHARACTER SET utf8mb4 NULL,
     `CreationTime` datetime(6) NOT NULL,
     `Expiration` datetime(6) NULL,
     `ConsumedTime` datetime(6) NULL,
-    `Data` longtext NOT NULL,
-    PRIMARY KEY (`Id`)
-);
+    `Data` longtext CHARACTER SET utf8mb4 NOT NULL,
+    CONSTRAINT `PK_PersistedGrants` PRIMARY KEY (`Id`)
+) CHARACTER SET=utf8mb4;
 
 CREATE TABLE `PushedAuthorizationRequests` (
     `Id` int NOT NULL AUTO_INCREMENT,
-    `ReferenceHashValue` varchar(64) NOT NULL,
+    `ReferenceHashValue` varchar(64) CHARACTER SET utf8mb4 NOT NULL,
     `Created` datetime(6) NOT NULL,
-    `Parameters` varchar(max) NOT NULL,
-    PRIMARY KEY (`Id`)
-);
+    `Parameters` longtext CHARACTER SET utf8mb4 NOT NULL,
+    CONSTRAINT `PK_PushedAuthorizationRequests` PRIMARY KEY (`Id`)
+) CHARACTER SET=utf8mb4;
 
 CREATE TABLE `ServerSideSessions` (
     `Id` int NOT NULL AUTO_INCREMENT,
-    `Key` varchar(100) NOT NULL,
-    `Scheme` varchar(100) NOT NULL,
-    `SubjectId` varchar(100) NOT NULL,
-    `SessionId` varchar(100) NULL,
-    `DisplayName` varchar(100) NULL,
+    `Key` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
+    `Scheme` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
+    `SubjectId` varchar(100) CHARACTER SET utf8mb4 NOT NULL,
+    `SessionId` varchar(100) CHARACTER SET utf8mb4 NULL,
+    `DisplayName` varchar(100) CHARACTER SET utf8mb4 NULL,
     `Created` datetime(6) NOT NULL,
     `Renewed` datetime(6) NOT NULL,
     `Expires` datetime(6) NULL,
-    `Data` varchar(max) NOT NULL,
-    PRIMARY KEY (`Id`)
-);
+    `Data` longtext CHARACTER SET utf8mb4 NOT NULL,
+    CONSTRAINT `PK_ServerSideSessions` PRIMARY KEY (`Id`)
+) CHARACTER SET=utf8mb4;
 
 CREATE UNIQUE INDEX `IX_DeviceCodes_DeviceCode` ON `DeviceCodes` (`DeviceCode`);
 
@@ -82,7 +84,7 @@ CREATE INDEX `IX_PersistedGrants_SubjectId_ClientId_Type` ON `PersistedGrants` (
 CREATE INDEX `IX_PersistedGrants_SubjectId_SessionId_Type` ON `PersistedGrants` (`SubjectId`, `SessionId`, `Type`);
 
 INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
-VALUES ('20260521090113_Grants', '10.0.7');
+VALUES ('20260529102500_Grants', '9.0.16');
 
 COMMIT;
 
