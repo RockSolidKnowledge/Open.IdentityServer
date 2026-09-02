e6b5399d2219820430fc66eb7bbb55049626cd95
andrewclymer <andy@rocksolidknowledge.com>
2026-09-02 23:23:18 +0100

    Refactored to put a service layer between the response generator and the store. This simplifies the response generator

--- src/Open.IdentityServer/test/Open.IdentityServer.UnitTests/ResponseHandling/PushedAuthorizationResponseGeneratorTests.cs
+++ src/Open.IdentityServer/test/Open.IdentityServer.UnitTests/ResponseHandling/PushedAuthorizationResponseGeneratorTests.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using AwesomeAssertions;
 using Microsoft.Extensions.Logging;
@@ -20,12 +18,9 @@ namespace Open.IdentityServer.UnitTests.ResponseHandling;
 
 public class PushedAuthorizationResponseGeneratorTests
 {
-    private readonly Mock<IPushedAuthorizationRequestStore> _store = new();
-    private readonly Mock<IHandleGenerationService> _handleGenerationService = new();
+    private readonly Mock<IPushedAuthorizationRequestService> service = new();
     private Mock<ILogger<PushedAuthorizationResponseGenerator>> _logger = new();
-    private Mock<TimeProvider> clock = new Mock<TimeProvider>();
-    private IdentityServerOptions options = new IdentityServerOptions();
-
+    
     private ValidatedAuthorizeRequest _request = new ValidatedAuthorizeRequest() { Raw = new NameValueCollection() };
 
     public PushedAuthorizationResponseGeneratorTests()
@@ -36,120 +31,106 @@ public class PushedAuthorizationResponseGeneratorTests
     [Fact]
     public async Task CreateResponseAsync_WhenCalled_ShouldMapRequestCorrectlyAndSendToStore()
     {
+        Uri expectedKey = new Uri($"{IdentityServerConstants.PushedAuthorizationRequest.UriRequestPrefix}232444234");
+        
+        _request.Client = new Client();
+        
         var sut = CreateSut();
         
         PushedAuthorizationMemento? storedInfo = null;
-        
-        _store.Setup(s => s.StorePushedAuthorizationRequestAsync( It.IsAny<PushedAuthorizationMemento>()))
-            .Callback<PushedAuthorizationMemento>(info => storedInfo = info);
-        
-        await sut.CreateResponseAsync(_request);
-        
-        VerifyPushedAuthorizeRequestMapping(storedInfo, _request);
-    }
-    
-    [Fact]
-    public async Task CreateResponseAsync_WhenCalled_ShouldConstructUriCorrectlyAndPassToStore()
-    {
-        string generatedUniquePart = "sdufbsibdvibv";
-        
-        _handleGenerationService.Setup(g => g.GenerateAsync()).ReturnsAsync(generatedUniquePart);
-        
-        var sut = CreateSut();
 
-        string? passedId = null;
-        
-        _store.Setup(s => s.StorePushedAuthorizationRequestAsync( It.IsAny<PushedAuthorizationMemento>()))
-            .Callback<PushedAuthorizationMemento>(memento => passedId = memento.Key);
-        
-        await sut.CreateResponseAsync(_request);
-        
-        passedId.Should().NotBeNull();
-        passedId.Should().Be(IdentityServerConstants.PushedAuthorizationRequest.UriRequestPrefix + generatedUniquePart);
-    }
-    
-    [Fact]
-    public async Task CreateResponseAsync_WhenCalled_ShouldSetExpirationBasedOnOptions()
-    {
-        string generatedUniquePart = "sdufbsibdvibv";
-
-        _request.Client = new Client();
-        
-        DateTime expectedExpiration;
-        DateTime now = new DateTime(2027, 3, 2, 13, 10, 20);
-        DateTimeOffset spiedExpiration = now;
-        
-        options.PushedAuthorization.Expiration = TimeSpan.FromSeconds(90);
-
-        clock.Setup(c => c.GetUtcNow()).Returns(now);
-        expectedExpiration = now.Add(options.PushedAuthorization.Expiration);
-        
-        _handleGenerationService.Setup(g => g.GenerateAsync()).ReturnsAsync(generatedUniquePart);
-        
-        var sut = CreateSut();
-       
-        _store.Setup(s => s.StorePushedAuthorizationRequestAsync( It.IsAny<PushedAuthorizationMemento>()))
-            .Callback<PushedAuthorizationMemento>(memento => spiedExpiration = memento.ValidUntil);
-        
-        await sut.CreateResponseAsync(_request);
-
-        spiedExpiration.Should().Be(expectedExpiration);
-    }
-    
-    [Fact]
-    public async Task CreateResponseAsync_WhenCalled_ShouldSetExpirationBasedOnClientProperties()
-    {
-        string generatedUniquePart = "sdufbsibdvibv";
-
-        _request.Client = new Client()
-        {
-            PushedAuthorizationLifetime = TimeSpan.FromSeconds(30).Seconds
-        };
-        
-        DateTime expectedExpiration;
-        DateTime now = new DateTime(2027, 3, 2, 13, 10, 20);
-        DateTimeOffset spiedExpiration = now;
-
-        clock.Setup(c => c.GetUtcNow()).Returns(now);
-        int expectedDuration = (int)_request.Client.PushedAuthorizationLifetime;
-        expectedExpiration = now.AddSeconds(expectedDuration);
-        
-        _handleGenerationService.Setup(g => g.GenerateAsync()).ReturnsAsync(generatedUniquePart);
-        
-        var sut = CreateSut();
-       
-        _store.Setup(s => s.StorePushedAuthorizationRequestAsync( It.IsAny<PushedAuthorizationMemento>()))
-            .Callback<PushedAuthorizationMemento>( memento => spiedExpiration = memento.ValidUntil);
+        service.Setup(s => s.CreateAsync(_request.Client, _request.Raw))
+            .ReturnsAsync(new PushedAuthorization(expectedKey, TimeSpan.FromSeconds(20)));
         
         var response = await sut.CreateResponseAsync(_request);
 
-        response.Should().NotBeNull();
-        spiedExpiration.Should().Be(expectedExpiration);
-        response.Lifetime.Should().Be(expectedDuration);
-    }
-
-    [Fact]
-    public async Task CreateResponseAsync_WhenCalled_ShouldGenerateResponseCorrectly()
-    {
-        string generatedUniquePart = "sdufbsibdvibv";
-        string expectedUri =
-            $"{IdentityServerConstants.PushedAuthorizationRequest.UriRequestPrefix}{generatedUniquePart}";
-
-        _handleGenerationService.Setup(g => g.GenerateAsync()).ReturnsAsync(generatedUniquePart);
-
-        var sut = CreateSut();
-
-        PushedAuthorizationResponse? response = await sut.CreateResponseAsync(_request);
-
-        response.Should().NotBeNull();
-        response.Uri.Should().Be(expectedUri);
-        response.Lifetime.Should().Be(PushedAuthorizationResponseGenerator.DefaultRequestLifetimeInSeconds);
+        response!.Lifetime.Should().Be(20);
+        response.Uri.Should().Be(expectedKey.ToString());
     }
     
+    
+    // [Fact]
+    // public async Task CreateResponseAsync_WhenCalled_ShouldSetExpirationBasedOnOptions()
+    // {
+    //     string generatedUniquePart = "sdufbsibdvibv";
+    //
+    //     _request.Client = new Client();
+    //     
+    //     DateTime expectedExpiration;
+    //     DateTime now = new DateTime(2027, 3, 2, 13, 10, 20);
+    //     DateTimeOffset spiedExpiration = now;
+    //     
+    //     options.PushedAuthorization.Expiration = TimeSpan.FromSeconds(90);
+    //
+    //     clock.Setup(c => c.GetUtcNow()).Returns(now);
+    //     expectedExpiration = now.Add(options.PushedAuthorization.Expiration);
+    //     
+    //     _handleGenerationService.Setup(g => g.GenerateAsync()).ReturnsAsync(generatedUniquePart);
+    //     
+    //     var sut = CreateSut();
+    //    
+    //     _store.Setup(s => s.StorePushedAuthorizationRequestAsync( It.IsAny<PushedAuthorizationMemento>()))
+    //         .Callback<PushedAuthorizationMemento>(memento => spiedExpiration = memento.ValidUntil);
+    //     
+    //     await sut.CreateResponseAsync(_request);
+    //
+    //     spiedExpiration.Should().Be(expectedExpiration);
+    // }
+    
+    // [Fact]
+    // public async Task CreateResponseAsync_WhenCalled_ShouldSetExpirationBasedOnClientProperties()
+    // {
+    //     string generatedUniquePart = "sdufbsibdvibv";
+    //
+    //     _request.Client = new Client()
+    //     {
+    //         PushedAuthorizationLifetime = TimeSpan.FromSeconds(30).Seconds
+    //     };
+    //     
+    //     DateTime expectedExpiration;
+    //     DateTime now = new DateTime(2027, 3, 2, 13, 10, 20);
+    //     DateTimeOffset spiedExpiration = now;
+    //
+    //     clock.Setup(c => c.GetUtcNow()).Returns(now);
+    //     int expectedDuration = (int)_request.Client.PushedAuthorizationLifetime;
+    //     expectedExpiration = now.AddSeconds(expectedDuration);
+    //     
+    //     _handleGenerationService.Setup(g => g.GenerateAsync()).ReturnsAsync(generatedUniquePart);
+    //     
+    //     var sut = CreateSut();
+    //    
+    //     _store.Setup(s => s.StorePushedAuthorizationRequestAsync( It.IsAny<PushedAuthorizationMemento>()))
+    //         .Callback<PushedAuthorizationMemento>( memento => spiedExpiration = memento.ValidUntil);
+    //     
+    //     var response = await sut.CreateResponseAsync(_request);
+    //
+    //     response.Should().NotBeNull();
+    //     spiedExpiration.Should().Be(expectedExpiration);
+    //     response.Lifetime.Should().Be(expectedDuration);
+    // }
+
+    // [Fact]
+    // public async Task CreateResponseAsync_WhenCalled_ShouldGenerateResponseCorrectly()
+    // {
+    //     string generatedUniquePart = "sdufbsibdvibv";
+    //     string expectedUri =
+    //         $"{IdentityServerConstants.PushedAuthorizationRequest.UriRequestPrefix}{generatedUniquePart}";
+    //
+    //     _handleGenerationService.Setup(g => g.GenerateAsync()).ReturnsAsync(generatedUniquePart);
+    //
+    //     var sut = CreateSut();
+    //
+    //     PushedAuthorizationResponse? response = await sut.CreateResponseAsync(_request);
+    //
+    //     response.Should().NotBeNull();
+    //     response.Uri.Should().Be(expectedUri);
+    //     response.Lifetime.Should().Be(PushedAuthorizationResponseGenerator.DefaultRequestLifetimeInSeconds);
+    // }
+    
     [Fact]
-    public async Task CreateResponseAsync_WhenCalledAndStoreThrowsException_ShouldReturnNull()
+    public async Task CreateResponseAsync_WhenCalledAndServiceThrowsException_ShouldReturnNull()
     {
-        _store.Setup(s => s.StorePushedAuthorizationRequestAsync(It.IsAny<PushedAuthorizationMemento>()))
+        service.Setup(s => s.CreateAsync(It.IsAny<Client>(),It.IsAny<NameValueCollection>()))
             .ThrowsAsync(new Exception());
 
         var sut = CreateSut();
@@ -159,19 +140,8 @@ public class PushedAuthorizationResponseGeneratorTests
         response.Should().BeNull();
     }
     
-    private void VerifyPushedAuthorizeRequestMapping(PushedAuthorizationMemento? storedInfo, ValidatedAuthorizeRequest request)
-    {
-        storedInfo.Should().NotBeNull();
-        storedInfo.Parameters.Should().BeEquivalentTo(request.Raw);
-        storedInfo.Key.Should().NotBeEmpty();
-    }
-    
     private PushedAuthorizationResponseGenerator CreateSut()
     {
-        return new PushedAuthorizationResponseGenerator(_store.Object, 
-            _handleGenerationService.Object, 
-            clock.Object,
-            options,
-            _logger.Object);
+        return new PushedAuthorizationResponseGenerator(service.Object, _logger.Object);
     }
 }
--- src/Storage/src/Stores/IPushedAuthorizationRequestStore.cs
+++ src/Storage/src/Stores/IPushedAuthorizationRequestStore.cs
@@ -1,12 +1,32 @@
 // Copyright (c) 2026, Rock Solid Knowledge Ltd
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+using System;
 using System.Threading.Tasks;
 using Open.IdentityServer.Storage.Models;
 
 namespace Open.IdentityServer.Stores;
 
 #nullable enable
+
+/// <summary>
+/// Raised by a PAR store 
+/// </summary>
+public class PushedAuthorizationRequestStoreException : Exception
+{
+    /// <summary>
+    /// PAR Store Exception
+    /// </summary>
+    /// <param name="message">The error message</param>
+    public PushedAuthorizationRequestStoreException(string message):base(message) { }
+
+    /// <summary>
+    /// Par Store Exception
+    /// </summary>
+    /// <param name="message">The error message</param>
+    /// <param name="inner">Inner exception</param>
+    public PushedAuthorizationRequestStoreException(string message, Exception inner) : base(message, inner) { }
+}
 
 /// <summary>
 /// Models the persistence of a pushed authorization request.
