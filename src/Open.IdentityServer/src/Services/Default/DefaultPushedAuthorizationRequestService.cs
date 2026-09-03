d4a3733d8b5c73c6e4b306ce7b5a949805e8c467
andrewclymer <andy@rocksolidknowledge.com>
2026-09-03 14:42:38 +0100

    Ensure no sensitive authentication parameters are persisted

--- src/Open.IdentityServer/src/Services/Default/DefaultPushedAuthorizationRequestService.cs
+++ src/Open.IdentityServer/src/Services/Default/DefaultPushedAuthorizationRequestService.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -21,6 +23,8 @@ internal class DefaultPushedAuthorizationRequestService(
     {
         try
         {
+            parameters = RemoveAnyAuthenticationParameters(parameters);
+            
             string keyBody = await handleGeneration.GenerateAsync();
             string key = $"{IdentityServerConstants.PushedAuthorizationRequest.UriRequestPrefix}{keyBody}";
 
@@ -51,6 +55,18 @@ internal class DefaultPushedAuthorizationRequestService(
        
     }
 
+    private static readonly List<string> AuthenticationParameters = 
+        ["client_secret", "client_assertion","client_assertion_type"];
+    
+    private NameValueCollection RemoveAnyAuthenticationParameters(NameValueCollection src)
+    {
+       var dest = new NameValueCollection(src);
+        
+        AuthenticationParameters.ForEach(dest.Remove);
+        
+        return dest;
+    }
+
     public async Task<NameValueCollection?> ConsumeAsync(string key)
     {
         try
@@ -67,6 +83,7 @@ internal class DefaultPushedAuthorizationRequestService(
         catch (PushedAuthorizationRequestStoreException e)
         {
             logger.LogError("Failed to consume PAR request store error {key}:{exception}",key,e.Message);
+            throw;
         }
         catch (Exception e)
         {
